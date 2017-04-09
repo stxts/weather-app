170dabc7ba012729db80226539d6449dbd76adea
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-04-08 23:54:02 -0700

    mapped json to interface

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Response, Headers, RequestOptions } from '@angular/http';
-import 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import { App } from './app';
 
 @Injectable()
@@ -8,15 +8,16 @@ export class AppService {
 
   private testUrl = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?q=';
   private apiKey = '&appid=6cc3e287c4cede83bd9e993a2aaf7923'
+  private units = '&units=imperial'
 
   // dependency injection
 	constructor(
 		private http: Http
 	){}
 
-  getCurrentWeather(city){
-    return this.http.get(this.testUrl + city + this.apiKey)
-               .map(res => res.json());
+  getCurrentWeather(city): Observable<App>{
+    return this.http.get(this.testUrl + city + this.units + this.apiKey)
+               .map((response: Response) => <App>response.json());
   }	
 
   
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,24 @@
-export class App {
-	constructor(
-		public id?: number,
-		public name?: string
-		){}
+export interface App {
+	public id?: number,
+	public name?: string,
+	public weather: Weather[],
+	public wind: Wind
+}
+
+export interface Weather {
+	public main?: string,
+}
+
+export interface Wind {
+	public speed?: decimal,
+	public deg?: decimal
+}
+
+export interface Main {
+	public temp?: decimal,
+	public pressure?: number,
+	public humidity?: number,
+	public temp_min?: decimal,
+	public temp_max?: decimal
+
 }
