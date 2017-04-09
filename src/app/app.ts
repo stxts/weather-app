1c3d47ca930f7435979399b090dbb5a80dd2b6cd
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-04-09 10:03:04 -0700

    removed public from interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,24 +1,24 @@
 export interface App {
-	public id?: number,
-	public name?: string,
-	public weather: Weather[],
-	public wind: Wind
+	id?: number,
+ 	name?: string,
+	weather: Weather[],
+	wind: Wind
 }
 
 export interface Weather {
-	public main?: string,
+	main?: string,
 }
 
 export interface Wind {
-	public speed?: decimal,
-	public deg?: decimal
+	speed?: decimal,
+	deg?: decimal
 }
 
 export interface Main {
-	public temp?: decimal,
-	public pressure?: number,
-	public humidity?: number,
-	public temp_min?: decimal,
-	public temp_max?: decimal
+	temp?: decimal,
+	pressure?: number,
+	humidity?: number,
+	temp_min?: decimal,
+	temp_max?: decimal
 
 }
