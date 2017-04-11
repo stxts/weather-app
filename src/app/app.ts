0e922c8637e6477346c5cfc37e1c396d4a7abe4f
Songtham Tungkitkancharoen <songthamtung@gmail.com>
2017-04-10 22:20:22 -0700

    readded app.ts

---
+++ src/app/app.ts
@@ -0,0 +1,25 @@
+declare var decimal: any
+
+export interface App {
+	public id?: number,
+	public name?: string,
+	public weather: Weather[],
+	public wind: Wind
+}
+
+export interface Weather {
+	public main?: string,
+}
+
+export interface Wind {
+	public speed?: decimal,
+	public deg?: decimal
+}
+
+export interface Main {
+	public temp?: decimal,
+	public pressure?: number,
+	public humidity?: number,
+	public temp_min?: decimal,
+	public temp_max?: decimal
+}
